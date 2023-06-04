0aed9a6fe943fd159c236e597d5e7c5f2bed2514
Serhii-Saproshyn <112710917+Serhii-Saproshyn@users.noreply.github.com>
2023-06-04 22:30:55 +0000

    Deploying to gh-pages from @ Serhii-Saproshyn/try-vova@5236fd5474a933553a3fe98e01e06f5f2a6595e1 🚀

---
+++ static/js/544.dd64e57d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktry_vova=self.webpackChunktry_vova||[]).push([[544],{544:function(e,t,r){r.r(t);var n=r(87),s=r(184);t.default=function(){return(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)("h1",{children:"HomePage"}),(0,s.jsx)(n.rU,{to:"/tweets",children:"Tweets"})]})}}}]);
+//# sourceMappingURL=544.dd64e57d.chunk.js.map
